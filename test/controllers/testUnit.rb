83e646ce7687c24e8d5ee710861b793a933899da
crosende1 <crosende1@uc.cl>
2016-05-15 11:29:46 -0300

    testPrueba

--- test/controllers/testUnit.rb
+++ test/controllers/testUnit.rb
@@ -1,16 +1,32 @@
 
 require 'test_helper'
+require 'test/unit'
+require 'mates'
 
-class UsuarioTest < ActiveSupport::TestCase
+class testUnit < ActionController::TestCase
 
-def test "validar_usuario" do
+test "validar_usuario" do
     username = @tagsArreglo["data"][0]["username"]
     assert_equal(username, 'Ricardo')
 end
 
-def test "error400" do
-    username = @tagsArreglo["data"][0]["username"]
-    assert_equal(username, 'Ricardo')
+test "error400" do
+    get :httppartymetodo, access_token: "cata" 
+    assert true
+
 end
+
+test "prueba_fallida" do
+    assert false
+end
+
+test "suma" do
+    assert_equal 4, Mates.run("2+2")
+    assert_equal 4, Mates.run("1+3")
+    assert_equal 5, Mates.run("5+0")
+    assert_equal 0, Mates.run("-5 + 5")
+end
+
+
 
 end
