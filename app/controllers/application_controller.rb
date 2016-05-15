6d94b5db9e44e85a09d5ab2c183d4445534eb544
crosende1 <crosende1@uc.cl>
2016-05-15 13:32:27 -0300

    testUnitarios

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ end
 end
 
 
-render json: {metadata: {total: media_count["data"]["media_count"]}, posts: @tagsArreglo, version: '1.1.0'}
+render json: {metadata: {total: media_count["data"]["media_count"]}, posts: @tagsArreglo, version: '1.1.1'}
 
 
 end
